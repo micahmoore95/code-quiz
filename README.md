d7f52c62e21afebf4f3d5fc20b4bd8ab1774dc26
Moore <mooreart95@gmail.com>
2022-07-01 13:39:13 -0600

    button functionality and readme update

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ I am still adding questions but this will be a great place to go to refresh and 
 
 When you press the start button you will be met with some questions and answers to choose between. Good luck!
 
-![](assets%5Cimages%5CScreenshot-1.jpg)
+![Screenshot of Code Quiz](/assets/images/Screenshot-1.jpg)
+
+Link to Deployed Application:
+
+https://micahmoore95.github.io/code-quiz/
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 var startButton = document.querySelector('#start-btn');
 var startScreen = document.querySelector("#start-screen");
-var questionAnswerEl = document.querySelector('#question-answer');
+var question1 = document.querySelector('#question-1');
+var question2 = document.querySelector('#question-2');
+var question3 = document.querySelector('#question-3');
+var q1a1 = document.querySelector('#q1a1');
+var q1a2 = document.querySelector('#q1a2');
+var q1a3 = document.querySelector('#q1a3');
+var q1a4 = document.querySelector('#q1a4');
+var wrongText = document.querySelector('#wrong-text');
+var rightText = document.querySelector('#right-text');
 
 startButton.addEventListener('click', startQuiz);
 
@@ -8,5 +16,20 @@ function startQuiz() {
     startScreen.setAttribute("class", "hide");
     startButton.setAttribute("class", "hide");
 
-    questionAnswerEl.setAttribute("class", "show");
+    question1.setAttribute("class", "show");
 };
+
+function wrongAnswer() {
+    wrongText.setAttribute("class", "show");
+    setTimeout(function() {question1.setAttribute("class", "hide")},2000);
+}
+
+function rightAnswer(){
+    rightText.setAttribute("class", "show");
+    setTimeout(function() {question1.setAttribute("class", "hide")},2000);
+}
+
+q1a1.addEventListener('click', wrongAnswer)
+q1a2.addEventListener('click', wrongAnswer)
+q1a3.addEventListener('click', rightAnswer)
+q1a4.addEventListener('click', wrongAnswer)
